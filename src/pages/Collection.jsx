358a0aa39d6b8269ab47ce0c578d58e0062f2c0f
Ahmed Tahan <ahmed.tahan71@gmail.com>
2024-10-12 15:06:42 +0300

    Fixed filters boxes

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -98,33 +98,33 @@ const Collection = () => {
         >
           <p className="mb-3 text-sm font-medium">CATEGORIES</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
-            <p className="flex gap-2">
+            <label className="flex gap-2 cursor-pointer">
               <input
                 className="w-3"
                 type="checkbox"
                 value={"Men"}
                 onChange={toggleCategory}
-              />{" "}
+              />
               Men
-            </p>
-            <p className="flex gap-2">
+            </label>
+            <label className="flex gap-2 cursor-pointer">
               <input
                 className="w-3"
                 type="checkbox"
                 value={"Women"}
                 onChange={toggleCategory}
-              />{" "}
+              />
               Women
-            </p>
-            <p className="flex gap-2">
+            </label>
+            <label className="flex gap-2 cursor-pointer">
               <input
                 className="w-3"
                 type="checkbox"
                 value={"Kids"}
                 onChange={toggleCategory}
-              />{" "}
+              />
               Kids
-            </p>
+            </label>
           </div>
         </div>
         {/* -------------------------- SubCategory Filter ----------------------- */}
@@ -135,7 +135,7 @@ const Collection = () => {
         >
           <p className="mb-3 text-sm font-medium">TYPE</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
-            <p className="flex gap-2">
+            <label className="flex gap-2 cursor-pointer">
               <input
                 className="w-3"
                 type="checkbox"
@@ -143,8 +143,8 @@ const Collection = () => {
                 onChange={toggleSubCategory}
               />{" "}
               Topwear
-            </p>
-            <p className="flex gap-2">
+            </label>
+            <label className="flex gap-2 cursor-pointer">
               <input
                 className="w-3"
                 type="checkbox"
@@ -152,8 +152,8 @@ const Collection = () => {
                 onChange={toggleSubCategory}
               />{" "}
               Bottomwear
-            </p>
-            <p className="flex gap-2">
+            </label>
+            <label className="flex gap-2 cursor-pointer">
               <input
                 className="w-3"
                 type="checkbox"
@@ -161,7 +161,7 @@ const Collection = () => {
                 onChange={toggleSubCategory}
               />{" "}
               Winterwear
-            </p>
+            </label>
           </div>
         </div>
       </div>
