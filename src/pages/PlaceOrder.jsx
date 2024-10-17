63f67390fe6ebe1509f51ae7a8e5572c44a5df4f
Ahmed Tahan <ahmed.tahan71@gmail.com>
2024-10-17 13:10:27 +0300

    Added payment methods

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Title from "../components/Title";
 import CartTotal from "../components/CartTotal";
+import { assets } from "../assets/assets";
+import { ShopContext } from "../context/ShopContext";
 
 const PlaceOrder = () => {
+  const [method, setMethod] = useState("cod");
+
+  const { navigate } = useContext(ShopContext);
+
   return (
     <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
       {/* -------------------------- Left Side ------------------------- */}
@@ -66,6 +72,61 @@ const PlaceOrder = () => {
       <div className="mt-8">
         <div className="mt-8 min-w-80">
           <CartTotal />
+        </div>
+        <div className="mt-12">
+          <Title text1={"PAYMENT"} text2={"METHOD"} />
+          {/* ----------------------- Payment Method Selection -------------------------- */}
+          <div className="flex gap-3 flex-col lg:flex-row">
+            <div
+              onClick={() => setMethod("banqu_misr")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "banqu_misr" ? "bg-amber-600" : ""
+                }`}
+              ></p>
+              <img
+                className="h-5 mx-4"
+                src={assets.Banque_Misr}
+                alt="Banque Misr Logo"
+              />
+            </div>
+
+            <div
+              onClick={() => setMethod("nbe")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "nbe" ? "bg-amber-600" : ""
+                }`}
+              ></p>
+              <img className="h-5 mx-4" src={assets.NBE_logo} alt="NBE Logo" />
+            </div>
+
+            <div
+              onClick={() => setMethod("cod")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "cod" ? "bg-amber-600" : ""
+                }`}
+              ></p>
+              <p className="text-gray-500 text-sm font-medium mx-4">
+                CASH ON DELIVERY
+              </p>
+            </div>
+          </div>
+          <div className="w-full text-end mt-8">
+            <button
+              onClick={() => navigate("/orders")}
+              className="bg-dark-goldenrod text-white px-16 py-3 text-sm"
+            >
+              PLACE ORDER
+            </button>
+          </div>
         </div>
       </div>
     </div>
