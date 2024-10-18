bab22806a91eb5d74f71a4bd3147327c94e5a7c0
Ahmed Tahan <ahmed.tahan71@gmail.com>
2024-10-18 16:11:21 +0300

    Added login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [currentState, setCurrentState] = useState("Sign Up");
 
-export default Login
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form
+      // ------------------- Form logic for user authentication -------------------
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+    >
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+      {currentState === "Login" ? (
+        <> </>
+      ) : (
+        <input
+          className="w-full px-3 py-2 border border-gray-800"
+          type="text"
+          placeholder="Name"
+        />
+      )}
+      <input
+        className="w-full px-3 py-2 border border-gray-800"
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        className="w-full px-3 py-2 border border-gray-800"
+        type="password"
+        placeholder="Password"
+      />
+      <div className="w-full flex justify-between text-sm -mt-2">
+        <p className="cursor-pointer">Forgot Password?</p>
+        {currentState === "Login" ? (
+          <p
+            onClick={() => setCurrentState("Sign Up")}
+            className="cursor-pointer"
+          >
+            Create Account
+          </p>
+        ) : (
+          <p
+            onClick={() => setCurrentState("Login")}
+            className="cursor-pointer"
+          >
+            Login Here
+          </p>
+        )}
+      </div>
+    </form>
+  );
+};
+
+export default Login;
