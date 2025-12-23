087c79dc1ad0a466f75a4dbe695e3c2bbd8d028d
Ahmed Tahan <ahmed.tahan71@gmail.com>
2025-12-24 01:10:29 +0200

    Added user cart functions

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -33,6 +33,14 @@ const ShopContextProvider = (props) => {
       cartData[itemId][size] = 1;
     }
     setCartItems(cartData);
+
+    if(token){
+      try {
+        await axios.post(backendURL + "/api/cart/add", {itemId, size}, {headers: {token}});
+      } catch (error) {
+        toast.error(error.message);
+      }
+    }
   };
 
   const getCartCount = () => {
@@ -54,6 +62,15 @@ const ShopContextProvider = (props) => {
     cartData[itemId][size] = quantity;
 
     setCartItems(cartData);
+
+    if(token){
+      try {
+        await axios.post(backendURL + '/api/cart/update', {itemId, size, quantity}, {headers: {token}});
+      } catch (error) {
+        console.log(error);
+        toast.error(error.message);
+      }
+    }
   };
 
   const getCartAmount = () => {
@@ -85,6 +102,18 @@ const ShopContextProvider = (props) => {
     }
   };
 
+  const getUserCart = async (token) => {
+    try {
+      const response = await axios.post(backendURL + '/api/cart/get', {}, {headers: {token}});
+      if(response.data.success){
+        setCartItems(response.data.cartData);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  }
+
   useEffect(() => {
     getProductsData();
   }, []);
@@ -92,6 +121,7 @@ const ShopContextProvider = (props) => {
   useEffect(() => {
     if(!token && localStorage.getItem("token")){
       setToken(localStorage.getItem("token"));
+      getUserCart(localStorage.getItem("token"));
     }
   }, []);
 
